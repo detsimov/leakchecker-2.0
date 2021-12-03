c3201d61294c5772f6d129bed11772f2816fc604
Artyom <46855304+detsimov@users.noreply.github.com>
2021-12-03 05:33:23 +0300

    Поправлен BreachDao

--- app/src/main/java/com/detsimov/leakchecker/data/storage/dao/BreachDao.kt
+++ app/src/main/java/com/detsimov/leakchecker/data/storage/dao/BreachDao.kt
@@ -1,8 +1,10 @@
 package com.detsimov.leakchecker.data.storage.dao
 
+import androidx.room.Dao
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
+@Dao
 interface BreachDao {
 
     @Query("SELECT COUNT(id) from Breaches WHERE isSeen = :isSeen")
